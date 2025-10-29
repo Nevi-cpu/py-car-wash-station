49093501cb0a2f54287a3342073ea4afebb627dc
Andrii Usypiv <anriyusypiv@gmail.com>
2025-10-29 17:46:43 +0200

    1

--- app/main.py
+++ app/main.py
@@ -29,20 +29,25 @@ class CarWashStation:
         )
         return round(washing_price, 1)
 
+    def wash_single_car(self, car: Car) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
     def serve_cars(self, cars: list) -> float:
         total_income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
                 total_income += self.calculate_washing_price(car)
-                car.clean_mark = self.clean_power
+                # Викликаємо інший метод, щоб помити машину (виправлення помилки №1)
+                self.wash_single_car(car)
         return round(total_income, 1)
 
-    def wash_single_car(self, car: Car) -> None:
-        if car.clean_mark < self.clean_power:
-            car.clean_mark = self.clean_power
-
     def rate_service(self, assessment: float) -> None:
-        self.average_rating = (
-            (self.average_rating * self.count_of_ratings) + assessment
-        ) / (self.count_of_ratings + 1)
+        # Додаємо заокруглення (виправлення помилки №2)
+        self.average_rating = round(
+            (
+                (self.average_rating * self.count_of_ratings) + assessment
+            ) / (self.count_of_ratings + 1),
+            1
+        )
         self.count_of_ratings += 1
